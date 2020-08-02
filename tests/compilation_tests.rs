a137d33e6c0c50dc2dfd6bf0bfb0bb97ba023302
Kaelin Laundry <wasabifan@outlook.com>
2020-08-02 16:44:48 -0700

    Extract path concatenation into helper fn in tests

--- tests/compilation_tests.rs
+++ tests/compilation_tests.rs
@@ -1,19 +1,25 @@
+fn source_path(file_name: &str) -> String {
+    format!("tests/compilation_test_sources/{}", file_name)
+}
+
 #[test]
 fn compilation_tests() {
     let tests = trybuild::TestCases::new();
-    tests.pass("tests/compilation_test_sources/struct_implements_send_requires_send.rs");
-    tests.pass("tests/compilation_test_sources/struct_implements_send_sync_requires_send_sync.rs");
-    tests.pass("tests/compilation_test_sources/enum_implements_send_requires_send.rs");
-    tests.pass("tests/compilation_test_sources/enum_implements_send_sync_requires_send_sync.rs");
+    tests.pass(source_path("struct_implements_send_requires_send.rs"));
+    tests.pass(source_path(
+        "struct_implements_send_sync_requires_send_sync.rs",
+    ));
+    tests.pass(source_path("enum_implements_send_requires_send.rs"));
+    tests.pass(source_path(
+        "enum_implements_send_sync_requires_send_sync.rs",
+    ));
 
-    tests.compile_fail("tests/compilation_test_sources/missing_param.rs");
-    tests.compile_fail(
-        "tests/compilation_test_sources/struct_violates_send_custom_requires_send_sync.rs",
-    );
-    tests.compile_fail(
-        "tests/compilation_test_sources/struct_violates_send_custom_requires_send.rs",
-    );
-    tests.compile_fail(
-        "tests/compilation_test_sources/struct_violates_send_sync_rc_requires_send_sync.rs",
-    );
+    tests.compile_fail(source_path("missing_param.rs"));
+    tests.compile_fail(source_path(
+        "struct_violates_send_custom_requires_send_sync.rs",
+    ));
+    tests.compile_fail(source_path("struct_violates_send_custom_requires_send.rs"));
+    tests.compile_fail(source_path(
+        "struct_violates_send_sync_rc_requires_send_sync.rs",
+    ));
 }
